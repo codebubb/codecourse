97b0852fa22ae75c9f375a695d38e96338bb5227
James Bubb <jpbubb82@gmail.com>
2016-03-08 11:23:19 +0000

    Update security procedures.

--- run.php
+++ run.php
@@ -1,7 +1,28 @@
 <?php
+/*  Runs the code that is pased from the main page
+    Returns JSON object with :
+      "status"    : "OK" | "error",
+      "response"  : <result_of_code> | <error_message>
+*/
+header('Content-type: text/json');
+$general_error = "Oops! Something went wrong.  Review your code and try again.";
 
+/* Sanitise the input as best as can be */
+$any_sanity_errors    = false;
+$permitted_languages  = ['nodejs', 'php', 'python'];
 $code = $_GET['code'];
 $lang = $_GET['lang'];
+
+if (!in_array($lang, $permitted_languages)){
+  $any_sanity_errors = true;
+}
+
+if($any_sanity_errors){
+  echo json_encode(array("status" => "error", "response" => $general_error));
+  exit(0);
+}
+
+/* Passed sanity checks, setup code to run */
 
 // PHP doesn't like $ signs!
 if ($lang == 'php'){
@@ -16,7 +37,7 @@ $str = "cat << EOF | " . $lang ." 2>&1
 EOF
 ";
 
-// Test for error_get_last
+// Test for errors
 $error_test = "cat << EOF | " . $lang ." 2>&1 >/dev/null
 ".
   $code . "
@@ -24,10 +45,10 @@ $error_test = "cat << EOF | " . $lang ." 2>&1 >/dev/null
 EOF
 ";
 
-// Error state
+/* Run error testing code */
 $error = nl2br(shell_exec($error_test));
 
-header('Content-type: text/json');
+/* Final output either error_message or code_result */
 if($error){
   echo json_encode(array("status" => "error", "response" => $error));
 } else{
